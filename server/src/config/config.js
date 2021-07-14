4422123abe5bcd40a892c3aefea5a8607c7e5715
KellsCodes <nworjiifeanyi@gmail.com>
2021-07-14 13:34:54 +0100

    modified dev dependencies on the package.json file, included the db configuration in the configuration file

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -6,11 +6,12 @@ module.exports = {
     database: process.env.DB_NAME || 'poller',
     user: process.env.DB_USER || 'poller',
     password: process.env.DB_PASS || 'GleNiaweJujAacpBLpwuK4D8N',
+    connection_uri : "mongodb+srv://PollerAdmin:5QgIpdNxAbOPCCAT@poller.ijegt.mongodb.net/poller?retryWrites=true&w=majority",
     options: {
       dialect: process.env.DIALECT || 'sqlite',
       host: process.env.HOST || 'localhost',
       storage: './poller.sqlite3'
     }
   },
-  port: process.env.PORT || 8000
+  port: process.env.PORT || 8080
 }
