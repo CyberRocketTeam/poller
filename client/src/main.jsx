1f1e7b2d82098f9edb7e02bd7e690c4fbf2abeb6
sphade <sphade010@gmail.com>
2021-09-16 11:39:42 -1000

    structured files

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { Provider } from "./context";
+import { Provider } from "./components/context";
 
 ReactDOM.render(
   <React.StrictMode>
